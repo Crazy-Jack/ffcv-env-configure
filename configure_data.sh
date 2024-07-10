1c3a77f98ec6a92f5e4f248119ab5d3013f57df6
Tianqin Li <tianqinl@andrew.cmu.edu>
2024-07-10 15:53:54 -0700

    Update configure_data.sh

--- configure_data.sh
+++ configure_data.sh
@@ -1,1 +1,2 @@
 mkdir -p /workspace/data/ffcv_imagenet && cd /workspace/data/ffcv_imagenet && \
+/workspace/b2 account authorize && /workspace/b2 file download b2://ffcv-imagenet/in1k_train_500_0.50_90.ffcv train_500_0.50_90.ffcv
