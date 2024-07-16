7da2a2e2b66c5c24c0030e355467b07b30b1c81f
Tianqin Li <tianqinl@andrew.cmu.edu>
2024-07-16 02:48:40 -0700

    Update configure_from_conda_env.sh

--- configure_from_conda_env.sh
+++ configure_from_conda_env.sh
@@ -107,6 +107,10 @@ source activate /workspace/env/miniconda/envs/ffcv && \
 conda install nvidia/label/cuda-11.8.0::cuda-toolkit -y && \
 conda install pytorch==2.0.0 torchvision==0.15.0 pytorch-cuda=11.8 -c pytorch -c nvidia -y 
 
+# ffcv
+source activate /workspace/env/miniconda/envs/ffcv && \
+pip uninstall ffcv && \
+pip install /workspace/env/FFCV-SSL
 
 
 echo "#########################"
